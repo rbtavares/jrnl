c2ea9f6f14f824bbddf5f151564e5a5432cd467e
rbtavares <73722899+rbtavares@users.noreply.github.com>
2025-09-10 16:20:53 +0100

    base monorepo structure

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
