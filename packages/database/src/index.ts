1758ba63cceb2ed3ff38acbeed0cb56b7b07cf50
rbtavares <73722899+rbtavares@users.noreply.github.com>
2025-09-11 18:00:48 +0100

    formatting

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,2 @@
 export * from './schema/index.js';
 export { db } from './client.js';
-
--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -6,6 +6,10 @@ export const usersTable = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   email: text('email').notNull(),
   password: text('password').notNull(),
-  createdAt: text('created_at').notNull().default(sql`(datetime('now'))`),
-  updatedAt: text('updated_at').notNull().default(sql`(datetime('now'))`),
+  createdAt: text('created_at')
+    .notNull()
+    .default(sql`(datetime('now'))`),
+  updatedAt: text('updated_at')
+    .notNull()
+    .default(sql`(datetime('now'))`),
 });
