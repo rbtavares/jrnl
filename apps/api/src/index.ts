c311d55142e86263827876a4774d3244ee078749
rbtavares <73722899+rbtavares@users.noreply.github.com>
2025-09-11 18:14:24 +0100

    feat(api): integrate database for user management and update root route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,18 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { db, usersTable } from '@repo/database';
 
 const app = new Hono();
 
-app.get('/', c => {
-  return c.text('Hello Hono!');
+app.get('/', async c => {
+
+  await db.insert(usersTable).values({
+    email: Date.now().toString(),
+    password: Date.now().toString(),
+  });
+  const users = await db.select().from(usersTable);
+
+  return c.json({ users });
 });
 
 serve(
@@ -13,6 +21,6 @@ serve(
     port: 3000,
   },
   info => {
-    console.log(`Server is running on http://localhost:${info.port}`);
+    console.log(`API is listening on http://localhost:${info.port}`);
   }
 );
