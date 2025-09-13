84871209dbc59f45a1e5e9cd1becac82d1fe109f
rbtavares <73722899+rbtavares@users.noreply.github.com>
2025-09-13 15:28:04 +0100

    graceful shutdown for api service

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -11,6 +11,21 @@ app.get('/', async (c) => {
   return c.json({ users });
 });
 
-serve({ fetch: app.fetch, port: 3000 }, (info) => {
+const server = serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`API is listening on http://localhost:${info.port}`);
+});
+
+process.on('SIGINT', () => {
+  server.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 });
